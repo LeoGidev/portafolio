b31524c45946a5d7d29e05a47002f6bee3f6dfcb
LeoGidev <leogimenez.dev@gmail.com>
2023-12-27 14:37:40 -0300

    trabajando en admisnitrador

--- admin.php
+++ admin.php
@@ -4,14 +4,14 @@
     else {
         echo "<div class='alert alert-danger mt-4' role='alert'>
         <h4>Debes iniciar sesion</h4>
-        <p><a href='index.html'>Logeate para continuar</a></p></div>";
+        <p><a href='log.html'>Logeate para continuar</a></p></div>";
         exit;
     }
     // controla si la sesion expira
     $now = time();           
     if ($now > $_SESSION['expire']) {
         session_destroy();
-        header("Location: index.html");
+        header("Location: log.html");
         exit;
         }
 		
