46ba93126b8d9c5e8ee8bd195cec8eb6934d1137
LeoGidev <leogimenez.dev@gmail.com>
2024-01-17 22:08:35 -0300

    se mejoran stilos y se trabaja en foter

--- script.js
+++ script.js
@@ -50,4 +50,36 @@ options.forEach(option => {
   });
 });
 
+//efecto letras
+document.addEventListener('DOMContentLoaded', function(){
+  var words = ['DEVELOPER', 'D', 'DE', 'DEV', 'DEVE', 'DEVEL', 'DEVELO', 'DEVELOP', 'DEVELOPE', 'DEVELOPER'];
+  var currentIndex = 0;
+  var wordAnimation = document.getElementById('word-animation');
+
+  function changeWord() {
+      // Oculta el elemento actual
+      wordAnimation.style.opacity = 0;
+
+      
+          // Cambia la palabra
+          wordAnimation.textContent = words[currentIndex];
+          // Muestra el elemento
+          wordAnimation.style.opacity = 1;
+          // Actualiza el índice para la próxima palabra
+          currentIndex = (currentIndex + 1) % words.length;
+
+          // Ajusta el tiempo para la próxima palabra
+          if (currentIndex === 0) {
+              setTimeout(changeWord, 2000); // Si es la primera palabra, espera 2 segundos
+          } else {
+              setTimeout(changeWord, 100); // Para las otras palabras, espera 100 milisegundos
+          }
+      
+  }
+
+  // Inicia el cambio de palabra
+  changeWord();
+  setInterval(efecto, 10000);
+});
+
  
