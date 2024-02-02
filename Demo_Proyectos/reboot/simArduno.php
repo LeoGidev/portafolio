ae634795ae4e181fca1167fb326c5bb1d5607286
LeoGidev <leogimenez.dev@gmail.com>
2024-02-01 22:15:16 -0300

    se trabaja en proyectos

--- Demo_Proyectos/reboot/simArduno.php
+++ Demo_Proyectos/reboot/simArduno.php
@@ -1,7 +1,13 @@
 <?php
-$q=$_POST;
-$b=$_GET;
 
-echo $q;
-echo $b;
+$estado=$_POST["Ac"];
+$clave=$_POST['psw'];
+
+if($clave == '123'){
+    if($estado == 'RebootA' ){
+        echo 'Reboot exitoso';
+    }
+}
+
+
 ?>
