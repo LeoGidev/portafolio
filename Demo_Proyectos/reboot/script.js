a7af89975b5e289937f81a0faa37b46f5a95803a
LeoGidev <leogimenez.dev@gmail.com>
2024-02-01 22:07:47 -0300

    se trabaja en proyectos

--- Demo_Proyectos/reboot/script.js
+++ Demo_Proyectos/reboot/script.js
@@ -11,96 +11,17 @@ var tigB;
 var tigC;
 var tigD;
 
-function ValoresAjax()
-{
 
-nocache = "&nocache=" + Math.random() * 1000000;
-
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function()
-{
-
-        if (this.readyState == 4) {
-
-        if (this.status == 200) {
-
-        if (this.responseXML != null) {
-        if(this.responseXML.getElementsByTagName('ValuA')[0].childNodes[0].nodeValue === "checked"){
-         x=1;}else{ x=0;}
-         
-         if (x==1 && tig==1){
-alert("Reboot en EdgeA realizado con exito");tig=0; 
-mostrarbotones();}
-else if(x==0 && tig==1){
-alert("Password incorrecto, Reboot en EdgeA abortado");tig=0;
-mostrarbotones();}
-         if(this.responseXML.getElementsByTagName('ValuB')[0].childNodes[0].nodeValue === "checked"){
-         xB=1;}else{ xB=0;}
-         
-         if (xB==1 && tigB==1){
-alert("Reboot en EdgeB realizado con exito");tigB=0; 
-mostrarbotones();}
-else if(xB==0 && tigB==1){
-alert("Password incorrecto, Reboot en EdgeB abortado");tigB=0;
-mostrarbotones();}
-           if(this.responseXML.getElementsByTagName('ValuC')[0].childNodes[0].nodeValue === "checked"){
-         xC=1;}else{ xC=0;}
-         
-         if (xC==1 && tigC==1){
-alert("Reboot en EdgeC realizado con exito");tigC=0; 
-mostrarbotones();}
-else if(xC==0 && tigC==1){
-alert("Password incorrecto, Reboot en EdgeC abortado");tigC=0;
-mostrarbotones();}
-          if(this.responseXML.getElementsByTagName('ValuD')[0].childNodes[0].nodeValue === "checked"){
-         xD=1;}else{ xD=0;}
-         
-         if (xD==1 && tigD==1){
-alert("Reboot en EdgeD realizado con exito");tigD=0;
-mostrarbotones();	   }
-else if(xD==0 && tigD==1){
-alert("Password incorrecto, Reboot en EdgeD abortado");tigD=0;
-mostrarbotones();}						
-
-        
-       
-
-}}
-}
-
-}	
 
            
 
 
-request.open("GET", "simArduno.php" + nocache, true);
 
-request.send(null);
-
-setTimeout('ValoresAjax()', 1000);
-
-
-strRA = "";
-strRB = "";
-strRC = "";
-strRD = "";
-
-
-}
 
 
 function RebootA(){
 var user= prompt("clave de Reboot");
-
-nocache = "&nocache=" + Math.random() * 1000000;
-var request = new XMLHttpRequest();
-strRA = user;
-request.open("POST", "REBOOTER" +"&"+ strRA + "&RebootA" + nocache, true);
-request.send(null);
-user = "";
-strRA = "";
-    $.ajax({
+ $.ajax({
       url: 'simArduno.php',
       method: 'GET',
       data: { Ac:'RebootA'}, 
