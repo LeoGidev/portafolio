5fc45695efa1cec84b235fc90227cf915d62d080
LeoGidev <leogimenez.dev@gmail.com>
2024-02-05 15:23:46 -0300

    se trabaja en proyectos

--- Demo_Proyectos/Telemeter/scripts.js
+++ Demo_Proyectos/Telemeter/scripts.js
@@ -9,22 +9,27 @@ function SendText(){
     request.open('GET', 'SETEO' + '&pass=' + strPS + strText +  nocache, true);
     request.send(null);location.href ='main.htm';}
 
-function GetArduinoInputs(){
-    nocache = '&nocache=' + Math.random() * 1000000;
-    var request = new XMLHttpRequest(); 
-    request.onreadystatechange = function() {
-        if (this.readyState == 4) { 
-            if (this.status == 200) { if (this.responseXML != null) {
-                document.getElementById('TenL').innerHTML = this.responseXML.getElementsByTagName('button1')[0].childNodes[0].nodeValue;
-                document.getElementById('CorrL').innerHTML = this.responseXML.getElementsByTagName('button2')[0].childNodes[0].nodeValue;
-                document.getElementById('TenB').innerHTML = this.responseXML.getElementsByTagName('button3')[0].childNodes[0].nodeValue;
-                document.getElementById('CorrB').innerHTML = this.responseXML.getElementsByTagName('button4')[0].childNodes[0].nodeValue;
-                document.getElementById('TempA').innerHTML = this.responseXML.getElementsByTagName('button5')[0].childNodes[0].nodeValue;
-                document.getElementById('TempB').innerHTML = this.responseXML.getElementsByTagName('button6')[0].childNodes[0].nodeValue;
+    function GetArduinoInputs() {
+        var nocache = '&nocache=' + Math.random() * 1000000;
+        var request = new XMLHttpRequest();
+    
+        request.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    if (this.responseXML != null) {
+                        document.getElementById('TenL').innerHTML = this.responseXML.getElementsByTagName('button1')[0].childNodes[0].nodeValue;
+                        document.getElementById('CorrL').innerHTML = this.responseXML.getElementsByTagName('button2')[0].childNodes[0].nodeValue;
+                        document.getElementById('TenB').innerHTML = this.responseXML.getElementsByTagName('button3')[0].childNodes[0].nodeValue;
+                        document.getElementById('CorrB').innerHTML = this.responseXML.getElementsByTagName('button4')[0].childNodes[0].nodeValue;
+                        document.getElementById('TempA').innerHTML = this.responseXML.getElementsByTagName('button5')[0].childNodes[0].nodeValue;
+                        document.getElementById('TempB').innerHTML = this.responseXML.getElementsByTagName('button6')[0].childNodes[0].nodeValue;
+                    }
+                }
             }
-        }
+        };
+    
+        request.open('GET', 'ArduSimu.php?' + nocache, true);
+        request.send(null);
+        setTimeout(GetArduinoInputs, 5000);
     }
-} 
-request.open('GET', 'ajax_inputs' + nocache, true); 
-request.send(null);
-setTimeout('GetArduinoInputs()', 5000);}
+    
