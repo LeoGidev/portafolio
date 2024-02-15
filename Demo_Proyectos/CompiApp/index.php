b77dbc0165206bf9f1944fba236edc05e994bd41
LeoGidev <leogimenez.dev@gmail.com>
2024-02-15 20:51:27 -0300

    se agrega styles.css

--- Demo_Proyectos/CompiApp/index.php
+++ Demo_Proyectos/CompiApp/index.php
@@ -27,7 +27,7 @@ include '../recursos/barra.php';
 ?>
 </header>
 
-    <div>
+    <div class="container contenido">
         <iframe src="https://www.veed.io/embed/d334e8d6-78ab-47e5-82ca-bf3d05bb54ac" width="744" height="504" frameborder="0" title="Proyecto sin titulo" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
 </body>
