5fc3559af232c581947560cfb75a527ff79ea583
LeoGidev <leogimenez.dev@gmail.com>
2024-02-15 21:36:14 -0300

    se trabaja en barra de tareas y enlaces

--- Demo_Proyectos/recursos/barra.php
+++ Demo_Proyectos/recursos/barra.php
@@ -13,7 +13,7 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <h6 class="ml-3"> Proyectos</h6>
               <a class="dropdown-item" href="../Demo_Proyectos/Reboot/index.html">Device Remote Reboot</a>
-              <a class="dropdown-item" href="https://yplef.000webhostapp.com/" target="_blank">YPLEF web page</a>
+              <a class="dropdown-item" href="http://yplef.rf.gd/" target="_blank">YPLEF web page</a>
               <div class="dropdown-divider"></div>
               <h6 class="ml-3">Prácticas</h6>
               <a class="dropdown-item" href="../../Practicas/Paint/">Simple Paint Web</a>
