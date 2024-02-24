50e88e24896dbd7f5ab7759e847e72cd4cb69c9b
LeoGidev <leogimenez.dev@gmail.com>
2024-02-24 17:47:38 -0300

    se trabaja en prácticas

--- Practicas/recursos/barra.php
+++ Practicas/recursos/barra.php
@@ -12,16 +12,17 @@
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <h6 class="ml-3"> Proyectos</h6>
-              <a class="dropdown-item" href="../Reboot/">Device Remote Reboot</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/Reboot/">Device Remote Reboot</a>
               <a class="dropdown-item" href="http://yplef.rf.gd/" target="_blank">YPLEF web page</a>
               <a class="dropdown-item" href="../diagnostic/">Diagnostic</a>
-              <a class="dropdown-item" href="../Telemeter/">Device Telemeter </a>
-              <a class="dropdown-item" href="../CompiApp/">CompiApp(Python)</a>
-              <a class="dropdown-item" href="../TecnicAssistant/">Tecnic Assistant(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/Telemeter/">Device Telemeter </a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/CompiApp/">CompiApp(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/TecnicAssistant/">Tecnic Assistant(Python)</a>
               <div class="dropdown-divider"></div>
               <h6 class="ml-3">Prácticas</h6>
-              <a class="dropdown-item" href="../../Practicas/Paint/">Simple Paint Web</a>
-              <a class="dropdown-item" href="../../Practicas/CartondeRifa/">Carton de Rifas</a>
+              <a class="dropdown-item" href="../Paint/">Simple Paint Web</a>
+              <a class="dropdown-item" href="../CartondeRifa/">Carton de Rifas</a>
+              <a class="dropdown-item" href="../cudaricular/">Cuadricular Imágines</a>
             </div>
           </li>
           <li class="nav-item">
