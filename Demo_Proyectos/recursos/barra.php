23c0651c5e7aa5849c1bf5ac7894dc2e713f54de
LeoGidev <leogimenez.dev@gmail.com>
2024-02-27 19:46:24 -0300

    se trabaja en barras de navegación

--- Demo_Proyectos/recursos/barra.php
+++ Demo_Proyectos/recursos/barra.php
@@ -11,11 +11,10 @@
               Demos
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <h6 class="ml-3"> Proyectos</h6>
-              
-              <a class="dropdown-item" href="http://tecnop.rf.gd/" target="_blank">Tecnop web page</a>
-              <a class="dropdown-item" href="../Reboot/">Device Remote Reboot</a>
+              <h6 class="ml-3"> Proyectos</h6>              
+              <a class="dropdown-item" href="http://tecnop.rf.gd/" target="_blank">Tecnop web page</a>              
               <a class="dropdown-item" href="http://yplef.rf.gd/" target="_blank">YPLEF web page</a>
+              <a class="dropdown-item" href="../Reboot/">Device Remote Reboot</a>
               <a class="dropdown-item" href="../diagnostic/">Diagnostic</a>
               <a class="dropdown-item" href="../Telemeter/">Device Telemeter </a>
               <a class="dropdown-item" href="../CompiApp/">CompiApp(Python)</a>
