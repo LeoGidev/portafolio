a43d66ad8bb6c0cfbf8a713a7604a897f7e134bb
LeoGidev <leogimenez.dev@gmail.com>
2024-03-05 17:49:44 -0300

    se trabaja en proyecto Selenium

---
+++ Demo_Proyectos/SeleniumApp/index.php
@@ -0,0 +1,39 @@
+
+<DOCTYPE HTML>
+    <html lang="en">
+<head>
+<title>Leo Gimenez</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/314f7a8189.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300;400&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="img/icono.png" type="image/x-icon">
+  <link rel="shortcut icon" href="img/icono.png" type="image/x-icon">
+  </head>
+
+<body>
+    <header>
+        <!-- Barra de navegación -->
+        <?php
+include '../recursos/barra.php';       
+?>
+</header>
+    <div class="contenido2">
+    <h6> Compi App </6>
+    </div>
+    <div class="container contenido">
+        <iframe src="https://www.veed.io/embed/d334e8d6-78ab-47e5-82ca-bf3d05bb54ac" width="744" height="504" frameborder="0" title="Proyecto sin titulo" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>
+    <div class="contenido">
+    <a href="https://github.com/LeoGidev/CalcuadorMascaraRed" target="_blank"><img src="../../img/GitHub-logo.webp"></a>
+        </div>
+        <script src="script.js"></script>
+</body>
