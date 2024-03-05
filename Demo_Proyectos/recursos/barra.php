706c60e1541a11f3da64cbeea245be8a211eb4e1
LeoGidev <leogimenez.dev@gmail.com>
2024-03-05 17:55:06 -0300

    se corrigen barras

--- Demo_Proyectos/recursos/barra.php
+++ Demo_Proyectos/recursos/barra.php
@@ -19,6 +19,7 @@
               <a class="dropdown-item" href="../diagnostic/">Diagnostic</a>
               <a class="dropdown-item" href="../Telemeter/">Device Telemeter </a>
               <a class="dropdown-item" href="../CompiApp/">CompiApp(Python)</a>
+              <a class="dropdown-item" href="../SeleniumApp">SeleniumApp(Python)</a>
               <a class="dropdown-item" href="../TecnicAssistant/">Tecnic Assistant(Python)</a>
               <a class="dropdown-item" href="../TagsApp/">TagsApp(Python)</a>
               <div class="dropdown-divider"></div>
