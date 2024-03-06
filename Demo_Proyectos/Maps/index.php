dbbebb32f7a447d037d35ec04f2172cb347ccb15
LeoGidev <leogimenez.dev@gmail.com>
2024-03-05 21:32:49 -0300

    se arregla proyecto maps

--- Demo_Proyectos/Maps/index.php
+++ Demo_Proyectos/Maps/index.php
@@ -1,5 +1,6 @@
    
 <!DOCTYPE html><html>
+<head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
