07639152790c2ec4475757f02e22b964ae70ce34
LeoGidev <leogimenez.dev@gmail.com>
2024-03-08 20:45:48 -0300

    se trabaja en proyectos

--- Demo_Proyectos/velociraptorApp/index.php
+++ Demo_Proyectos/velociraptorApp/index.php
@@ -27,7 +27,7 @@ include '../recursos/barra.php';
 ?>
 </header>
     <div class="contenido2">
-    <h6> Compi App </6>
+    <h6> Velociraptor App </6>
     </div>
     <div class="container contenido">
     <iframe src="https://www.veed.io/embed/075a2fdf-b56c-4597-9ef8-5de8d834bbe5" width="744" height="504" frameborder="0" title="velociraptor" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
