4ef253abfc12de390e8001e6271926ee7dfc0350
LeoGidev <leogimenez.dev@gmail.com>
2024-03-15 18:58:37 -0300

    se trabaja en prácticas

---
+++ Practicas/Redes/index.php
@@ -0,0 +1,44 @@
+<DOCTYPE HTML>
+    <html lang="en">
+<head>
+<title>Leo Gimenez</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/314f7a8189.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300;400&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="img/icono.png" type="image/x-icon">
+  <link rel="shortcut icon" href="img/icono.png" type="image/x-icon">
+  </head>
+
+<body>
+    <header>
+        <!-- Barra de navegación -->
+        <?php
+include '../recursos/barra.php';       
+?>
+</header>
+<div class='container'>
+  <div class='row justify-content-center'>
+    <div class="col-12 contenido2">
+    <h4> Selector de Redes </h4>
+    </div>
+  </div>
+    
+  <div class="contenedor">
+            <div class="tras im1"><img src="im1.png" alt="redes" onclick="mostrar()"></div>
+            <div class=" tras red1"><img src="red1.png" alt="facebook"></div>
+            <div class="tras red2"><img src="red2.png" alt="instagram"></div>
+            <div class="tras red3"><img src="red3.png" alt="twitter"></div>
+            <div class="tras red4"><img src="red4.png" alt="tiktok"></div>
+            <div class="tras red5"><img src="red5.png" alt="youtube"></div>
+        </div>
+        <script src="script.js"></script>
+</body>
