9695f1855e5e3aa14113aae5a74611f97458b491
LeoGidev <leogimenez.dev@gmail.com>
2024-03-15 19:29:00 -0300

    se trabaja en prácticas

---
+++ Practicas/bannergiratorio/index.php
@@ -0,0 +1,47 @@
+<DOCTYPE HTML>
+    <html lang="en">
+<head>
+<title>Leo Gimenez</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/314f7a8189.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300;400&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="img/icono.png" type="image/x-icon">
+  <link rel="shortcut icon" href="img/icono.png" type="image/x-icon">
+  </head>
+
+<body>
+    <header>
+        <!-- Barra de navegación -->
+        <?php
+include '../recursos/barra.php';       
+?>
+</header>
+<div class='container'>
+  <div class='row justify-content-center'>
+    <div class="col-12 contenido2">
+    <h4> Selector de Redes </h4>
+    </div>
+  </div>
+    <div class="info invermov">
+        <div class="col-12 container-fluid info-banner ml-3 mr-3" id="B1"><table><tr><td colspan="2"><h2>Banner 1</h2></td> <tr><td><img src="img/auto1.webp" alt="automovil"></td><td><p>Descripción del banner 1</p></td></tr></table></div>
+        <div class="col-12 container-fluid info-banner ml-3 mr-3" id="B2"><table><tr><td colspan="2"><h2>Banner 2</h2></td> <tr><td><img src="img/auto2.webp" alt="automovil"></td><td><p>Descripción del banner 2</p></td></tr></table></div>
+        <div class="col-12 container-fluid info-banner ml-3 mr-3" id="B3"><table><tr><td colspan="2"><h2>Banner 3</h2></td> <tr><td><img src="img/auto3.webp" alt="automovil"></td><td><p>Descripción del banner 3</p></td></tr></table></div>
+        <div class="col-12 container-fluid info-banner ml-3 mr-3" id="B4"><table><tr><td colspan="2"><h2>Banner 4</h2></td> <tr><td><img src="img/auto4.webp" alt="automovil"></td><td><p>Descripción del banner 4</p></td></tr></table></div>
+      </div>
+      <div class="contenido">
+    <a href="https://github.com/LeoGidev/banner-giratorio" target="_blank"><img src="../../img/GitHub-logo.webp"></a>
+        </div>
+        <script src="script.js"></script>
+
+</body>
+
+</html>
