30b85832783b93116493be7d4134cab9c16c7a6a
LeoGidev <leogimenez.dev@gmail.com>
2024-03-26 16:09:24 -0300

    se trabaja en prácticas

--- Practicas/Tetris/index.php
+++ Practicas/Tetris/index.php
@@ -69,7 +69,7 @@ include '../recursos/barra.php';
 
     
     <div class="contenido3 container">
-    <a href="https://github.com/LeoGidev/reloj" target="_blank"><img src="../../img/GitHub-logo.webp"></a>
+    <a href="https://github.com/LeoGidev/Tetris" target="_blank"><img src="../../img/GitHub-logo.webp"></a>
     </div>
 
 
