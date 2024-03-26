eff29e368fd32a8bb4e60682d91c76f702fedd85
LeoGidev <leogimenez.dev@gmail.com>
2024-03-26 16:21:52 -0300

    se trabaja en prácticas

--- Practicas/recursos/barra.php
+++ Practicas/recursos/barra.php
@@ -16,7 +16,7 @@
               <a class="dropdown-item" href="http://yplef.rf.gd/" target="_blank">YPLEF web page</a>
               <a class="dropdown-item" href="../../Demo_Proyectos/Maps/">Maps monitor</a>
               <a class="dropdown-item" href="../../Demo_Proyectos/Reboot/">Device Remote Reboot</a>
-              <a class="dropdown-item" href="../diagnostic/">Diagnostic</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/diagnostic/">Diagnostic</a>
               <a class="dropdown-item" href="../../Demo_Proyectos/Telemeter/">Device Telemeter </a>
               <a class="dropdown-item" href="../../Demo_Proyectos/CompiApp/">CompiApp(Python)</a>
               <a class="dropdown-item" href="../../Demo_Proyectos/SeleniumApp/">SeleniumApp(Python)</a>
