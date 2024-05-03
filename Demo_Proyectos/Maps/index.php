350eb99edb6df7fb54b76d8e2f95a6378ec0520a
LeoGidev <leogimenez.dev@gmail.com>
2024-05-03 20:12:12 -0300

    se trabaja en proyectos

--- Demo_Proyectos/Maps/index.php
+++ Demo_Proyectos/Maps/index.php
@@ -16,6 +16,8 @@
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
       <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="styles.css">
+      <link rel="icon" href="../../img/ico.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../../img/ico.ico" type="image/x-icon">
       
       <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
