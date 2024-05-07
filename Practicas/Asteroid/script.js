53ffa26076032fc52e1f42d3fbf3df295210412e
LeoGidev <leogimenez.dev@gmail.com>
2024-05-07 16:42:10 -0300

    se trabaja en prácticas

--- Practicas/Asteroid/script.js
+++ Practicas/Asteroid/script.js
@@ -282,10 +282,10 @@ function gamewin(){
             var texto1 = "Recarga la página para reinciar";
             var anchoTexto = ctx.measureText(texto).width;
             var anchoTexto1 = ctx.measureText(texto1).width;
-            console.log("canvas:", canvas.width, "texto:", anchoTexto1, "punto:", canvas.width/2-anchoTexto1/2);
+            console.log("canvas:", canvas.width, "texto:", anchoTexto1, "punto:", (canvas.width-anchoTexto1)/2);
             ctx.font="20px Arial";
-            ctx.fillText(texto, canvas.width / 2- anchoTexto/2, canvas.height / 2 - 28);
-            ctx.fillText(texto1, canvas.width / 2 - anchoTexto1/2, canvas.height / 2 + 24);
+            ctx.fillText(texto, (canvas.width/2) - (anchoTexto/2), canvas.height / 2 - 28);
+            ctx.fillText(texto1, (canvas.width/2) - (anchoTexto1/2), canvas.height / 2 + 24);
         }else{
             var texto = "¡¡HAS GANADO!!";
             var texto1 = "Presiona R para reiniciar";
