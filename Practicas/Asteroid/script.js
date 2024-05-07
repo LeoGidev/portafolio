0ba6d588f850b8ff317cdf5f5a73c96993535b23
LeoGidev <leogimenez.dev@gmail.com>
2024-05-07 16:11:23 -0300

    se trabaja en prácticas

--- Practicas/Asteroid/script.js
+++ Practicas/Asteroid/script.js
@@ -277,12 +277,15 @@ function gamewin(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = RED;
         if(window.innerWidth < 900){
-            ctx.font="10px Arial";
+            ctx.font="30px Arial";
+            ctx.fillText("¡¡HAS GANADO!!", canvas.width / 2, canvas.height / 2 - 24);
+            ctx.fillText("Presione R para reiniciar", canvas.width / 2, canvas.height / 2 + 24);
         }else{
         ctx.font = "48px Arial";
-        }
         ctx.fillText("¡¡HAS GANADO!!", canvas.width / 2 - 150, canvas.height / 2 - 24);
         ctx.fillText("Presione R para reiniciar", canvas.width / 2 - 260, canvas.height / 2 + 24);
+        }
+        
 
     }
 
