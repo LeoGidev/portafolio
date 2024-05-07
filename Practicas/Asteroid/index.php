43c1b682aef8a565a574e65098b019a7de38f17c
LeoGidev <leogimenez.dev@gmail.com>
2024-05-07 15:54:52 -0300

    se trabaja en prácticas

--- Practicas/Asteroid/index.php
+++ Practicas/Asteroid/index.php
@@ -42,7 +42,7 @@ include '../recursos/barra.php';
     
 </div>
 
-<div id = "controls" class="contenido2 controles">
+<div id = "controls" class="contenido2 controles" hidden>
   <button class="izq" id="movleft">◄</button>
   <button class="shoot" id="sho">֍</button>
   <button class="der" id="movright">►</button>
